d4436532b3c13a8add50300bc051659394371c60
filipnykanen <43749749+filipnykanen@users.noreply.github.com>
2019-10-20 22:03:13 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-unit-testing';
