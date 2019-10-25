c34872e09cbb71bdfdc4853f2832c0242520a228
Filip Nykänen <nykanenf@arcada.fi>
2019-10-25 20:36:12 +0300

    added a simple form with some tests to the app

---
+++ src/app/form1/form1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form1',
+  templateUrl: './form1.component.html',
+  styleUrls: ['./form1.component.css']
+})
+
+export class Form1Component {
+  formtitle = 'Please rate and comment this beautiful app:';
+}
